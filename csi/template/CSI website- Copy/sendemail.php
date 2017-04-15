747cd3a48a5f53f7ec649d4f77dbcdd6271f2733
shashanknayak96 <shashank96.nayak@gmail.com>
2017-04-15 20:51:31 +0530

    changed email-id

--- csi/template/CSI website- Copy/sendemail.php
+++ csi/template/CSI website- Copy/sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'saumikpatel32@gmail.com';//replace with your email
+$to   		= 'yagnesh.devashrayee@universalcoe.edu.in';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
